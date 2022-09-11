7b392c46f7c3f695f7126eda0fd5c7ccc4b99811
Rinhard javed <rinhardjaved@gmail.com>
2022-09-11 10:39:03 -0400

    fill data

--- data/qna.js
+++ data/qna.js
@@ -2,75 +2,110 @@ const questionsNode =
 [
   {
     question: "How are Node based web servers are different from traditional web servers?",
-    choices: ["Node based server process request much faster than traditional server.","Node based server uses a single threaded model and can services much larger number of requests than traditional server like Apache HTTP Server.","There is not much difference between the two.", "None of the above."],
-    answer:1
+    choice1: "Node based server process request much faster than traditional server.",
+    choice2:"Node based server uses a single threaded model and can services much larger number of requests than traditional server like Apache HTTP Server.",
+    choice3:"There is not much difference between the two.",
+    choice4:"None of the above.", 
+    answer:2,
   },
   {
     question: "By default, npm installs any dependency in the local mode.",
-    choices: ["True", "False"],
-    answer:0
+    choice1:"True",
+    choice2:"False",
+    answer:1,
   },
   {
     question: "Which of the following is true with respect to Node.",
-    choices: ["Every API of Node js are asynchronous.","Node being a single thread, and uses async function calls to maintain the concurrency.","Node thread keeps an event loop and whenever any task get completed, it fires the corresponding event which signals the event listener function to get executed.","All of the above."],
-    answer:3
+    choice1:"Every API of Node js are asynchronous.",
+    choice2:"Node being a single thread, and uses async function calls to maintain the concurrency." ,
+    choice3:"Node thread keeps an event loop and whenever any task get completed, it fires the corresponding event which signals the event listener function to get executed.",
+    choice4:"All of the above." ,
+
+    answer:4,
   },
   {
     question: " Which of the following is true about global objects in Node applications?",
-    choices: ["Global objects are global in nature and they are available in all modules.","Global objects are nore required to be included in application, rather they can be used directly.","Both of the above.","None of the above."],
-    answer:2
-  }
+    choice1:"Global objects are global in nature and they are available in all modules.",
+    choice2:"Global objects are nore required to be included in application, rather they can be used directly.",
+    choice3:"Both of the above.",
+    choice4:"None of the above.",
+    answer:3,
+  },
 ]
 
 const questionsJavascript = 
 [
   {
-    question: "Inside which HTML element do we put the JavaScript?",
-    choices: ["js tag","script tag","body tag","javascript tag"],
-    answer:1
+    question:"Inside which HTML element do we put the JavaScript?",
+    choice1: "js tag",
+    choice2:"script tag",
+    choice3:"body tag",
+    choice4:"javascript tag",
+    answer:2,
   },
 
 
 
   {
     question: "What is the correct JavaScript syntax to change the content of the HTML element below?",
-    choices: ["document.getElementById('demo').innerHTML='hello world!';","#demo.innerHTML='hello world!';" ],
-    answer:0
+    choice1:"document.getElementById('demo').innerHTML='hello world!';",
+    choice2:"#demo.innerHTML='hello world!';",
+    choice3:"document.querySelector('demo').innerHTML = 'hello world';",
+    choice4:"None of the above",
+    answer:1,
   },
   {
     question: "What is the correct syntax for referring to an external script called 'xxx.js'?",
-    choices: ["<script href = 'xxx.js'>","<script name = 'xxx.js'>", "<script src = 'xxx.js'>"],
-    answer:0
+    choice1:"<script href = 'xxx.js'>",
+    choice2:"<script name = 'xxx.js'>",
+    choice3:"<script src = 'xxx.js'>",
+    choice4:"<link src = 'xxx.js'>",
+    answer:1,
   },
   {
-    question: "",
-    choices: "",
-    answer:1
-  }
+    question: "How does a FOR loop start?",
+    choice1:"for i = 1 to 5",
+    choice2:"for (i <= 5; i++)",
+    choice3:"for (i = 0; i <= 5; i++)  ",
+    choice4:"for (i = 0; i <= 5)",
+    answer:3,
+  },
 ]
 
 const questionsPython = 
 [
   {
-    question: "",
-    choices: "",
-    answer:1
+    question:"What is a correct syntax to output 'Hello World' in Python?",
+    choice1: "echo 'Hello World'",
+    choice2:"echo('Hello World');",
+    choice3:"print('Hello World')  ",
+    choice:"p('Hello World')",
+    answer:3,
   },
   {
-    question: "",
-    choices: "",
-    answer:1
+    question:"What is the correct file extension for Python files?",
+    choice1: ".pyt",
+    choice2:".py",
+    choice3:".pyth",
+    choice:".pt",
+    answer:2,
   },
   {
-    question: "",
-    choices: "",
-    answer:1
+    question:"What is the correct syntax to output the type of a variable or object in Python?",
+    choice1:"print(typeof(x))",
+    choice2:"print(typeOf(x))",
+    choice3:"print(typeof x)",
+    choice:"print(type(x))  ",
+    answer:4,
   },
   {
-    question: "",
-    choices: "",
-    answer:1
-  }
+    question:"Which method can be used to remove any whitespace from both the beginning and the end of a string?",
+    choice1:"len()",
+    choice2:"ptrim()",
+    choice3:"strip()",
+    choice:"trim()",
+    answer:3,
+  },
 ]
 
 
@@ -78,29 +113,35 @@ const questionsPython =
 const questionsReact = 
 [
   {
-    question: "",
-    choices: "",
-    answer:1
+    question: "What is the correct command to create a new React project?",
+    choice1:"npm create-react-app",
+    choice2:"npm create-react-app myReactApp",
+    choice3:"npx create-react-app",
+    choice:"npx create-react-app myReactApp",
+    answer:4,
   },
   {
-    question: "",
-    choices: "",
-    answer:1
+    question:"What command is used to start the React local development server?",
+    choice1:"npm build",
+    choice2:"npm run dev",
+    choice3:"npm serve",
+    choice:"npm start  ",
+    answer:4,
   },
   {
-    question: "",
-    choices: "",
-    answer:1
+    question:"What is the default local host port that a React development server uses?",
+    choice1:"3500",
+    choice2:"3000  ",
+    choice3:"5000",
+    choice:"8080",
+    answer:2,
   },
   {
-    question: "",
-    choices: "",
-    answer:1
-  }
+    question:"What is the children prop?",
+    choice1:"A property that lets you pass data to child components",
+    choice2:"A property that adds child values to state",
+    choice3: "A property that lets you set an object as a property",
+    choice:"A property that lets you nest components in other components  ",
+    answer:4
+  },
 ]
-
-function getQuestionNode() {
-  for ( let i = 0; i < questionsNode.length; i++ ) {
-    let question = questions[i].question;
-  }
-}
--- js/game.js
+++ js/game.js
@@ -34,6 +34,8 @@ const startGame = document.getElementById('start-button')
 
 
 
+
+
 /*-------------------------------- Functions ----------------------------*/
 
 
